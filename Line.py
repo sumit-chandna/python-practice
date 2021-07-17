a8f0a42b0384c02f22605940716f6514d8a0a486
sumit <sumit.chandna@fastcurveservices.com>
2021-07-17 13:31:49 +0530

    udemy  learn

--- Line.py
+++ Line.py
@@ -7,9 +7,11 @@ class Line:
         self.coordinate2 = coordinate2
 
     def distance(self):
-        return math.sqrt(
-            math.pow(self.coordinate2[0] - self.coordinate1[0], 2) + math.pow(self.coordinate2[1] - self.coordinate1[1],
-                                                                              2))
+        x1, y1 = self.coordinate1
+        x2, y2 = self.coordinate2
+        return math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
 
     def slope(self):
-        return (self.coordinate2[1] - self.coordinate1[1]) / (self.coordinate2[0] - self.coordinate1[0])
+        x1, y1 = self.coordinate1
+        x2, y2 = self.coordinate2
+        return (y2 - y1) / (x2 - x1)
